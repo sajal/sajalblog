b000a1324e32c88908bad287b4fdf62992ef0da0
Sajal Kayan <sajal@turbobytes.com>
2016-02-05 21:10:28 +0700

    sign each html page when deploy

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,6 @@ rm -rfv public/*
 hugo --theme=hyde --uglyUrls=true
 cp -r public/* ../sajal.github.io/
 cp ../sajal.github.io/post.xml ../sajal.github.io/feed.xml
+echo "Signing"
+find ../sajal.github.io/ | grep '\.html' | xargs -n1 -iX  ./signhtml.sh 'X'
 echo "Update done"
---
+++ signhtml.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+tmpfile=$(mktemp)
+echo "using $tmpfile for $1"
+echo "
+
+-->" > $tmpfile
+cat $1 >> $tmpfile
+echo "
+<!--
+" >> $tmpfile
+
+gpg --default-key BF15828F  --clearsign $tmpfile
+
+echo "<!--
+" > $1
+
+cat "$tmpfile.asc" >> $1
+
+echo "
+-->" >> $1
+
+rm $tmpfile
+rm "$tmpfile.asc"
